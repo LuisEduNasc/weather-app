8ee7c561e9f88db72a98ec8acce12d92f22d8364
LuisEduNasc <luis.nacimento@cravehq.com>
2023-04-20 21:47:38 -0300

    fix(url): fix url for heroku

--- src/api/cities.ts
+++ src/api/cities.ts
@@ -54,8 +54,7 @@ export interface IWeatherResponse {
 
 export const fetchCities = async (searchValue: string): Promise<ICityResponse[]> => {
   const response = await axios({
-    baseURL: process.env.NODE_ENV == 'production' ? 'https://weather-leis-app.herokuapp.com' : 'localhost:3000',
-    url: `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=50&appid=${process.env.REACT_APP_API_KEY}`,
+    url: `${process.env.NODE_ENV === 'production' ? 'https://cors-anywhere.herokuapp.com/' : ''}http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=50&appid=${process.env.REACT_APP_API_KEY}`,
     method: 'get',
   }).then(({ data }) => {
     const cities: ICityResponse[] = data;
@@ -67,8 +66,7 @@ export const fetchCities = async (searchValue: string): Promise<ICityResponse[]>
 
 export const fetchWeather = async ({lat, lon, temp = 'imperial'}: {lat: number | undefined, lon: number | undefined, temp: string | undefined}): Promise<IWeatherResponse> => {
   const response = await axios({
-      baseURL: process.env.NODE_ENV == 'production' ? 'https://weather-leis-app.herokuapp.com' : 'localhost:3000',
-      url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=${temp}`,
+      url: `${process.env.NODE_ENV === 'production' ? 'https://cors-anywhere.herokuapp.com/' : ''}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=${temp}`,
       method: 'get'
     }).then(({ data }) => {
       const weather: IWeatherResponse = data;
